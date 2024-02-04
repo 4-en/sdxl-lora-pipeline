0f1cc68cf92ae07a2baddecafe9de319fdbfd88d
4-en <ben.schumacher0@gmail.com>
2024-02-04 13:22:41 +0100

    add escape for comma in metadata

--- source/create_metadata.py
+++ source/create_metadata.py
@@ -2,6 +2,28 @@
 
 import os
 
+def to_csv_field(text: str, seperator: str=',') -> str:
+    """
+    Convert a string to a csv field
+    :param text: string
+    :param seperator: seperator to be used in the csv field
+    :return: csv field
+    """
+    text = text.replace('"', '""')
+
+    if seperator in text and not (text.startswith('"') and not text.startswith('""')):
+        return f'"{text}"'
+    return text
+def to_csv_row(columns: list, seperator: str=',') -> str:
+    """
+    Convert a list of strings to a csv row
+    :param columns: list of strings
+    :param seperator: seperator to be used in the csv row
+    :return: csv row
+    """
+    return seperator.join([to_csv_field(column, seperator) for column in columns])
+    
+
 def create_metadata(directory:str, text) -> None:
     """
     Create a metadata.csv file for the dataset
@@ -21,7 +43,8 @@ def create_metadata(directory:str, text) -> None:
     with open(directory + '/metadata.csv', 'w') as f:
         f.write('file_name,text\n')
         for filename in filenames:
-            f.write(f'{filename},{text}\n')
+            row = to_csv_row([filename, text])
+            f.write(row + '\n')
 
     print(f'metadata.csv file created in {directory}')
 
